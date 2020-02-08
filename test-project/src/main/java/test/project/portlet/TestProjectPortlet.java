6f980d954ed0d94969077a7c28c3fb76e428af8a
phosamani2 <phosamani2@IN5CG8367FQC.CSCMWS.CSCMWS.COM>
2020-02-08 11:31:53 +0530

    first commit

---
+++ test-project/src/main/java/test/project/portlet/TestProjectPortlet.java
@@ -0,0 +1,26 @@
+package test.project.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author phosamani2
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=test-project Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class TestProjectPortlet extends MVCPortlet {
+}
